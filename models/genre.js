8ded2cc635f1f81e6b4ace2cc110d486fe60657b
Adolfo Herrera <valherdolf@gmail.com>
2023-08-24 18:10:26 -0400

    Fix comment

--- models/genre.js
+++ models/genre.js
@@ -6,7 +6,7 @@ const GenreSchema = new Schema({
     name: { type: String, required: true, minLength: 3, maxLength: 100 }
 });
 
-// Virtual for book's URL
+// Virtual for genre's URL
 GenreSchema.virtual("url").get(function () {
     // We don't use an arrow function as we'll need the this object
     return `catalog/genre/${this._id}`;
