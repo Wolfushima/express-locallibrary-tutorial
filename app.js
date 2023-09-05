fc435368602ab9317baac913686c2e177cdcd118
Adolfo Herrera <valherdolf@gmail.com>
2023-09-05 17:47:51 -0400

    fix: Import 'mongoose'

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.use(
   );
 
 // Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
 const dev_db_url = "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
